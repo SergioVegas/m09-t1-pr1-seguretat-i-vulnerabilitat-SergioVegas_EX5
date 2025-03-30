66d24b51b399637a46310008cbaac1a93776f1ee
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-03-31 00:22:05 +0200

    d

--- Ex5/Messages.cs
+++ Ex5/Messages.cs
@@ -8,7 +8,7 @@ namespace Ex5
 {
     public static class Messages
     {
-        public static string Menu = "MENÚ PRINCIPAL\n"+ "1. Registre d'usuari\n" + "2. Verificació de dades\n" + "3. Encriptació/Desencriptació RSA\n" + "4. Sortir\n" + "Selecciona una opció:";
+        public static string Menu = "Menú Principal\n"+ "1. Registre d'usuari\n" + "2. Verificació de dades\n" + "3. Encriptació/Desencriptació RSA\n" + "4. Sortir\n" + "Selecciona una opció:";
         public static string InvalidOption = "Opció no vàlida. Torna a intentar.";
         public static string UserName = "Introdueix el nom d'usuari:";
         public static string UserPassword = "Introdueix la contrasenya:";
